0b297ff5bf88bcb1473810e3b7018c822f6e2e9e
jgregorio <gregoriojesus0@gmail.com>
2021-08-01 18:32:18 +0200

    Thread fundamentals Thread optimization Thread sharing

---
+++ thread-data-sharing/src/main/java/Stack.java
@@ -0,0 +1,12 @@
+public class Stack {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        int res = sum(1, 2);
+    }
+
+    private static int sum(int a, int b) {
+        int s = a + b;
+        return s;
+    }
+}
---
+++ thread-fundamentals/src/main/java/CreateThreadExtendingThread.java
@@ -0,0 +1,13 @@
+public class CreateThreadExtendingThread {
+    public static void main(String[] args) throws InterruptedException {
+        Thread rt = new RunableThread();
+        rt.start();
+    }
+
+    static class RunableThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("We are in thread " + Thread.currentThread().getName());
+        }
+    }
+}
